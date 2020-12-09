559743c4e35f2da5b679b4063183ae3d598aaf1d
Val <60119764+saliringval@users.noreply.github.com>
2020-12-09 01:35:16 -0800

    Create category_data.dart

---
+++ lib/src/data/category_data.dart
@@ -0,0 +1,50 @@
+import '../models/category_model.dart';
+
+final categories = [
+  Category(
+    numberofItems: 150,
+    imagePath: "assets/images/roses.jpg",
+    categoryName: "Roses"
+  ),
+  Category(
+    numberofItems: 1500,
+    imagePath: "assets/images/sunflower.jpg",
+    categoryName: "Sunflower"
+  ),
+  Category(
+    numberofItems: 450,
+    imagePath: "assets/images/cactus.jpg",
+    categoryName: "Cactus"
+  ),
+  Category(
+    numberofItems: 100,
+    imagePath: "assets/images/aloe.jpg",
+    categoryName: "Aloe"
+  ),
+    Category(
+    numberofItems: 110,
+    imagePath: "assets/images/aglaonema.jpg",
+    categoryName: "Aglaonema"
+  ),
+    Category(
+    numberofItems: 15,
+    imagePath: "assets/images/orchids.jpg",
+    categoryName: "Orchids"
+  ),
+    Category(
+    numberofItems: 30,
+    imagePath: "assets/images/tulips.jpg",
+    categoryName: "Tulips"
+  ),
+    Category(
+    numberofItems: 50,
+    imagePath: "assets/images/yucca.jpg",
+    categoryName: "Yucca"
+  ),
+    Category(
+    numberofItems: 14,
+    imagePath: "assets/images/zz plant.jpg",
+    categoryName: "ZZ Plant"
+  ),
+
+];
