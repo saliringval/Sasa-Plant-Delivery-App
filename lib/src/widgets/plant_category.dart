17dc8fe858eff04c1cddb4494abef1287134e463
Val <60119764+saliringval@users.noreply.github.com>
2020-12-09 01:46:37 -0800

    Create plant_category.dart

---
+++ lib/src/widgets/plant_category.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'plant_card.dart';
+import '../data/category_data.dart';
+import '../models/category_model.dart';
+
+class PlantCategory extends StatelessWidget{
+
+  final List<Category>_categories = categories;
+  
+  @override
+  Widget build(BuildContext context){
+    return Container(
+      height: 80.0,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: _categories.length,
+      itemBuilder: (BuildContext context, int index){
+        return PlantCard(
+          categoryName: _categories[index].categoryName,
+          imagePath: _categories[index].imagePath,
+          numberofItems: _categories[index].numberofItems,
+        );
+      },
+  
+      ),
+    );
+  }
+}
