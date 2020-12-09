d87755ffc351d7e327744ab0188786cecfbabc88
Val <60119764+saliringval@users.noreply.github.com>
2020-12-09 01:35:42 -0800

    Create plant_data.dart

---
+++ lib/src/data/plant_data.dart
@@ -0,0 +1,22 @@
+import '../models/plant_model.dart';
+
+final plants = [
+  Plant(
+    id:"1",
+    name:"Red Roses",
+    imagePath: "assets/images/rose.jpg",
+    category:"1",
+    price:80.0,
+    discount:20.0,
+    ratings: 87.0,
+  ),
+  Plant(
+    id:"2",
+    name:"Triumph Tulips",
+    imagePath: "assets/images/triumph.jpg",
+    category:"2",
+    price:45.0,
+    discount:20.0,
+    ratings: 65.0,
+  ),
+];
